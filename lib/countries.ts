194beb26d6b6cc83a3bf7f0fb8e6ee372ffdd10a
Max Baravy <maximkatut@gmail.com>
2022-06-09 12:16:30 -0400

    add countries request for all and particular

---
+++ lib/countries.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {
+  BASE_URL,
+  COUNTRIES_ROUTES,
+} from "../utils/constants/countries.constants";
+
+const clientApi = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const getAllCountries = async () => {
+  let countries;
+  await clientApi
+    .get(COUNTRIES_ROUTES.ALL)
+    .then((res) => res.data)
+    .then((data) => {
+      countries = data;
+    });
+  return countries;
+};
+
+export const getAllCountryIds = async () => {
+  let paths;
+  await clientApi
+    .get(COUNTRIES_ROUTES.ALL)
+    .then((res) => res.data)
+    .then((data) => {
+      paths = data.map((country: any) => {
+        return { params: { id: country.cca2.toLowerCase() } };
+      });
+    });
+  return paths;
+};
+
+export const getCoutry = async (cca2: string) => {
+  let country;
+  await clientApi
+    .get(`${COUNTRIES_ROUTES.ALPHA}/${cca2}`)
+    .then((res) => res.data)
+    .then((data) => {
+      country = data[0];
+    });
+  return country;
+};
