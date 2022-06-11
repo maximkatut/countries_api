9673a0f60a8eb764b76e0a5d6890fe1a590d42e3
Max Baravy <maximkatut@gmail.com>
2022-06-10 20:47:44 -0400

    custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      "dark-blue": "hsl(209, 23%, 22%)",
+      "very-dark-blue": "hsl(207, 26%, 17%)",
+      text: "hsl(200, 15%, 8%)",
+      "dark-grey": "hsl(0, 0%, 52%)",
+      "light-grey": "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    screens: {
+      sm: "375px",
+    },
   },
   plugins: [],
   darkMode: "class",
