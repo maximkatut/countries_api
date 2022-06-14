276bc579c7913ebd3dd28a023a63190bdc0a5366
Max Baravy <maximkatut@gmail.com>
2022-06-13 21:37:38 -0400

    too much pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 300,
   images: {
     domains: ["flagcdn.com", "upload.wikimedia.org"],
   },
