1f167af1bba9bc989d6f2321e85e6a74262ca636
Max Baravy <maximkatut@gmail.com>
2022-06-15 13:37:59 -0400

    add remove query button for input search

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,11 @@ import Link from "next/link";
 
 const Header = ({ home }: { home: boolean }) => {
   return (
-    <header className="transition flex bg-white dark:bg-dark-blue justify-between font-extrabold h-20 items-center px-20 shadow-md mb-11 -mx-20">
-      <Link href={"/"} className={home ? "pointer-events-none" : ""}>
-        <h1
-          className={`font-extrabold text-lg md:text-2xl ${
-            !home && "cursor-pointer"
-          }`}
-        >
-          Where in the world?
-        </h1>
+    <header className="transition flex bg-white dark:bg-dark-blue justify-between font-extrabold h-20 items-center px-10 md:px-20 shadow-md mb-11 -mx-10 md:-mx-20">
+      <Link passHref href={home ? "#" : "/"}>
+        <a className={`${home && "cursor-default"}`}>
+          <h1 className={`font-extrabold text-lg md:text-2xl`}>Where in the world?</h1>
+        </a>
       </Link>
       <DarkModeToggle />
     </header>
--- components/searchForm.tsx
+++ components/searchForm.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
+import { useRef } from "react";
 import { useStore } from "../store";
 
 interface IProps {
-  setQuery: (query: string) => void;
+  setQuery: (x: string) => void;
+  query: string;
 }
 
-const SearchForm = ({ setQuery }: IProps) => {
-  const handleOnChange = ({ target }: React.ChangeEvent<HTMLFormElement>) => {
+const SearchForm = ({ setQuery, query }: IProps) => {
+  const isDark = useStore((state) => state.isDark);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleChangeEvent = ({ target }: React.ChangeEvent<HTMLFormElement>) => {
     setQuery(target.value);
   };
+  const handleButtonClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setQuery("");
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
 
-  const isDark = useStore((state) => state.isDark);
   return (
-    <form className="mb-10" onChange={handleOnChange}>
+    <form className="mb-10" onChange={handleChangeEvent}>
       <label className="relative">
         <span className="absolute left-8 top-0">
           <Image
@@ -22,7 +33,17 @@ const SearchForm = ({ setQuery }: IProps) => {
             alt="Search input"
           />
         </span>
+        <button className={`absolute right-8 top-0 ${query === "" && "hidden"}`} onClick={handleButtonClick}>
+          <Image
+            className="opacity-50"
+            src={isDark ? "/images/xmark-dark.svg" : "/images/xmark.svg"}
+            width={16}
+            height={16}
+            alt="Search input"
+          />
+        </button>
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search for a country..."
           className="dark:bg-dark-blue p-5 w-[300px] md:w-[485px] pl-20 shadow-md text-sm rounded-md"
